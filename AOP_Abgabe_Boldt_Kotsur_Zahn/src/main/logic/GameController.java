4dcffbfd8705e5cd4894d044bef1f97eb52da495
somegithubuser245 <New_username_001@protonmail.com>
2024-08-17 21:25:08 +0200

    initial commit player and gameController classes

--- AOP_Abgabe_Boldt_Kotsur_Zahn/src/main/logic/GameController.java
+++ AOP_Abgabe_Boldt_Kotsur_Zahn/src/main/logic/GameController.java
@@ -1,5 +1,60 @@
 package main.logic;
 
+import main.gui.*;
+import main.data.*;
+
 public class GameController {
+	private int spielerAnzahl;
+	public boolean fertigGewurfelt;
+	
+	private Player[] playerList;
+	private Combos[] ComboKlassen;
+	public String currentPlayer;
+	//private int currentRound;
+	
+	public GameController(int spielerAnzahl) {
+		initGame(spielerAnzahl);
+		
+		this.fertigGewurfelt = false;
+		
+		//TO-DO : ComboKlassen initialisieren
+	}
+	
+	private void initGame(int spielerAnzahl) { //updates die spielerAnzahl und sets playerList Groesse
+		this.spielerAnzahl = spielerAnzahl;
+		playerList = new Player[this.spielerAnzahl];
+		
+		for(int i = 0; i < spielerAnzahl; i++) {
+			playerList[i] = new Player("test " + i);
+		}
+	}
+	
+	public void getSchowCombination() {
+		//wuerfel. ...
+		//gui. ...
+	}
+	
+	public void chooseCombinationOrSave() {
+		//wuerfel. ...
+		//gui. ...
+	}
+	
+	private void gameOver() {
+		
+	}
+	
+	private void nextPlayer() {
+		
+	}
+	
+	private void checkPlayerDone() {
+		
+	}
+	
+	private void starteBerechnung() {
+		
+	}
+	
+	
 	
 }
